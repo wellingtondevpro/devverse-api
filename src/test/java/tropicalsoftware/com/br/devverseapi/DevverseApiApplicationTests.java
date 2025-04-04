db6c5704b3385242bfb3c27d5cac981de86ab71f
Wellington Cesar <dev.wellington.io@gmail.com>
2025-04-03 23:50:48 -0300

    First commit

---
+++ src/test/java/tropicalsoftware/com/br/devverseapi/DevverseApiApplicationTests.java
@@ -0,0 +1,13 @@
+package tropicalsoftware.com.br.devverseapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevverseApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
